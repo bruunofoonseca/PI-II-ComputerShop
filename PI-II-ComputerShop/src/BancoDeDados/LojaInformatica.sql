697677678d389a3a037babc9a7f69999d2ec4c0e
bruunofoonseca <bruuno.foonseca@hotmail.com>
2017-06-15 15:03:18 -0300

    Ajuste na criação do banco de dados

--- PI-II-ComputerShop/src/BancoDeDados/LojaInformatica.sql
+++ PI-II-ComputerShop/src/BancoDeDados/LojaInformatica.sql
@@ -1,49 +1,49 @@
 create table cliente(
-IdCli int generated always as identity (start with 1, increment by 1) primary key,
-Nome varchar(50) not null,
-Sexo varchar (20),
-Status boolean,
-DataNasc Date not null,
-EstadoCivil varchar(30) not null,
-CPF varchar (30) not null,
-Tel varchar (30),
-Cel varchar (30),
-Email varchar(30),
-Logradouro varchar(50) not null,
-Numero varchar(30) not null,
-Complemento varchar(30),
-CEP varchar(10) not null,
-Bairro varchar(30) not null,
-Cidade varchar(30) not null,
-Estado varchar(30) not null
+    IdCli int generated always as identity (start with 1, increment by 1) primary key,
+    Nome varchar(50) not null,
+    Sexo varchar (20),
+    Status boolean,
+    DataNasc Date not null,
+    EstadoCivil varchar(30) not null,
+    CPF varchar (30) not null,
+    Tel varchar (30),
+    Cel varchar (30),
+    Email varchar(30),
+    Logradouro varchar(50) not null,
+    Numero varchar(30) not null,
+    Complemento varchar(30),
+    CEP varchar(10) not null,
+    Bairro varchar(30) not null,
+    Cidade varchar(30) not null,
+    Estado varchar(30) not null
 );
 
 create table produto(
-IdProd int generated always as identity (start with 1, increment by 1) primary key,
-NomeProd varchar(30) not null,
-Fabricante varchar(30) not null,
-TipoProd varchar(30) not null,
-Quantidade int,
-Status boolean,
-Valor float
+    IdProd int generated always as identity (start with 5000, increment by 5) primary key,
+    NomeProd varchar(30) not null,
+    Fabricante varchar(30) not null,
+    TipoProd varchar(30) not null,
+    Quantidade int,
+    Status boolean,
+    Valor float
 );
 
 create table ItensPedidos(
-IdItem int generated always as identity (start with 1, increment by 1) primary key,
-IdCli int,
-IdProd int,
-Quantidade int,
-SubTotal float,
-foreign key (IdCli) references cliente(IdCli),
-foreign key (IdProd) references produto(IdProd)
+    IdItem int generated always as identity (start with 2000, increment by 2) primary key,
+    IdProd int,
+    IdPedido int,
+    Quantidade int,
+    SubTotal float,
+    foreign key (IdProd) references produto(IdProd),
+    foreign key (idPedido) references pedido(IdPedido)
 );
 
 create table pedido(
-IdPedido int generated always as identity (start with 1, increment by 1) primary key,
-IdItem int,
-DataComp Date,
-Nome varchar (50),
-NomeProd varchar(30),
-Valor float,
-foreign key (IdItem) references ItensPedidos(IdItem)
+    IdPedido int generated always as identity (start with 1300, increment by 3) primary key,
+    IdCli int,
+    DataComp Date,
+    Nome varchar (50),
+    NomeProd varchar(30),
+    Valor float,
+    foreign key (IdCli) references cliente(IdCli)
 );
